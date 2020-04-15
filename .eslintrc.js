96a80ae20df1037f8ac0b3fa5781818cc3fc17fa
Ivan <lysko1999i@gmail.com>
2020-04-15 17:01:51 +0300

    created repo for task

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'react/state-in-constructor': [1, 'never'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-extraneous-dependencies': [
+      2,
+      { devDependencies: ['**/test.tsx', '**/test.ts'] },
+    ],
+    '@typescript-eslint/indent': [2, 2],
+  },
+  globals: {
+    window: true,
+    document: true,
+    localStorage: true,
+    FormData: true,
+    FileReader: true,
+    Blob: true,
+    navigator: true,
+  },
+};
---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = (): JSX.Element => (
+  <div>
+    <h1>Start</h1>
+  </div>
+);
+
+export default App;
